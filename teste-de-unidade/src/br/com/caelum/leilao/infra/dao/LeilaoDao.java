7dae6a28fd749a1fc158cd1e142129dc9fba9408
Matheus <matheus.svb>
2019-07-27 20:17:02 -0300

    AULA_01 COMPLETA

--- teste-de-unidade/src/br/com/caelum/leilao/infra/dao/LeilaoDao.java
+++ teste-de-unidade/src/br/com/caelum/leilao/infra/dao/LeilaoDao.java
@@ -14,15 +14,19 @@ import java.util.List;
 import br.com.caelum.leilao.dominio.Lance;
 import br.com.caelum.leilao.dominio.Leilao;
 import br.com.caelum.leilao.dominio.Usuario;
+import br.com.caelum.leilao.repositorio.RepositorioDeLeiloes;
 
-public class LeilaoDao {
+public class LeilaoDao implements RepositorioDeLeiloes{
 
 	private Connection conexao;
 
+	public static String teste() {
+		return "teste";
+	}
+
 	public LeilaoDao() {
 		try {
-			this.conexao = DriverManager.getConnection(
-					"jdbc:mysql://localhost/mocks", "root", "");
+			this.conexao = DriverManager.getConnection("jdbc:mysql://localhost/mocks", "root", "");
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
@@ -41,74 +45,76 @@ public class LeilaoDao {
 			ps.setString(1, leilao.getDescricao());
 			ps.setDate(2, new java.sql.Date(leilao.getData().getTimeInMillis()));
 			ps.setBoolean(3, leilao.isEncerrado());
-			
+
 			ps.execute();
-			
+
 			ResultSet generatedKeys = ps.getGeneratedKeys();
-	        if (generatedKeys.next()) {
-	            leilao.setId(generatedKeys.getInt(1));
-	        }
-			
-			for(Lance lance : leilao.getLances()) {
+			if (generatedKeys.next()) {
+				leilao.setId(generatedKeys.getInt(1));
+			}
+
+			for (Lance lance : leilao.getLances()) {
 				sql = "INSERT INTO LANCES (LEILAO_ID, USUARIO_ID, VALOR) VALUES (?,?,?);";
 				PreparedStatement ps2 = conexao.prepareStatement(sql);
 				ps2.setInt(1, leilao.getId());
 				ps2.setInt(2, lance.getUsuario().getId());
 				ps2.setDouble(3, lance.getValor());
-				
+
 				ps2.execute();
 				ps2.close();
-				
+
 			}
-			
+
 			ps.close();
-			
+
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
 		}
-		
+
 	}
-	
+
 	public List<Leilao> encerrados() {
 		return porEncerrado(true);
 	}
-	
+
 	public List<Leilao> correntes() {
 		return porEncerrado(false);
 	}
-	
+
 	private List<Leilao> porEncerrado(boolean status) {
 		try {
 			String sql = "SELECT * FROM LEILAO WHERE ENCERRADO = " + status + ";";
-			
+
 			PreparedStatement ps = conexao.prepareStatement(sql);
 			ResultSet rs = ps.executeQuery();
-			
+
 			List<Leilao> leiloes = new ArrayList<Leilao>();
-			while(rs.next()) {
+			while (rs.next()) {
 				Leilao leilao = new Leilao(rs.getString("descricao"), data(rs.getDate("data")));
 				leilao.setId(rs.getInt("id"));
-				if(rs.getBoolean("encerrado")) leilao.encerra();
-				
-				String sql2 = "SELECT VALOR, NOME, U.ID AS USUARIO_ID, L.ID AS LANCE_ID FROM LANCES L INNER JOIN USUARIO U ON U.ID = L.USUARIO_ID WHERE LEILAO_ID = " + rs.getInt("id");
+				if (rs.getBoolean("encerrado"))
+					leilao.encerra();
+
+				String sql2 = "SELECT VALOR, NOME, U.ID AS USUARIO_ID, L.ID AS LANCE_ID FROM LANCES L INNER JOIN USUARIO U ON U.ID = L.USUARIO_ID WHERE LEILAO_ID = "
+						+ rs.getInt("id");
 				PreparedStatement ps2 = conexao.prepareStatement(sql2);
 				ResultSet rs2 = ps2.executeQuery();
-				
-				while(rs2.next()) {
+
+				while (rs2.next()) {
 					Usuario usuario = new Usuario(rs2.getInt("id"), rs2.getString("nome"));
 					Lance lance = new Lance(usuario, rs2.getDouble("valor"));
-					
+
 					leilao.propoe(lance);
 				}
 				rs2.close();
 				ps2.close();
-				
+
 				leiloes.add(leilao);
-				
+
 			}
 			rs.close();
 			ps.close();
-			
+
 			return leiloes;
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
@@ -116,7 +122,8 @@ public class LeilaoDao {
 	}
 
 	public void atualiza(Leilao leilao) {
-		
+
+		System.out.println();
 		try {
 			String sql = "UPDATE LEILAO SET DESCRICAO=?, DATA=?, ENCERRADO=? WHERE ID = ?;";
 			PreparedStatement ps = conexao.prepareStatement(sql);
@@ -130,6 +137,8 @@ public class LeilaoDao {
 			throw new RuntimeException(e);
 		}
 	}
-	
-	public int x() { return 10; }
+
+	public int x() {
+		return 10;
+	}
 }
--- teste-de-unidade/src/br/com/caelum/leilao/servico/EncerradorDeLeilao.java
+++ teste-de-unidade/src/br/com/caelum/leilao/servico/EncerradorDeLeilao.java
@@ -4,14 +4,19 @@ import java.util.Calendar;
 import java.util.List;
 
 import br.com.caelum.leilao.dominio.Leilao;
-import br.com.caelum.leilao.infra.dao.LeilaoDao;
+import br.com.caelum.leilao.repositorio.RepositorioDeLeiloes;
 
 public class EncerradorDeLeilao {
 
 	private int total = 0;
 
+	private final RepositorioDeLeiloes dao;
+
+	public EncerradorDeLeilao(RepositorioDeLeiloes dao) {
+		this.dao = dao;
+	}
+
 	public void encerra() {
-		LeilaoDao dao = new LeilaoDao();
 		List<Leilao> todosLeiloesCorrentes = dao.correntes();
 
 		for (Leilao leilao : todosLeiloesCorrentes) {
